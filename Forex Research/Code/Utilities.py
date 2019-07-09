601c4aeffba69979745ee76899e3936db1adc776
JackBrady <jack.brady4227@gmail.com>
2019-07-09 16:50:44 -0500

    Add files via upload

---
+++ Forex Research/Code/Utilities.py
@@ -0,0 +1,10 @@
+
+# Snippet from Advances in Financial Machine Learning by Dr. Marcos Lopez de Prado
+
+def get_daily_volatility(close,span0=100):
+    df0=close.index.searchsorted(close.index-pd.Timedelta(days=1))
+    df0=df0[df0>0]
+    df0=pd.Series(close.index[df0-1], index = close.index[close.shape[0]-df0.shape[0]:])
+    df0=close.loc[df0.index]/close.loc[df0.values].values-1
+    df0=df0.ewm(span=span0).std() 
+    return df0
